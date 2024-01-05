fe6bb7bac743c395bbffbdc389233a7c6e632811
Saleh Alkhalifa <alkhalifas@thallium.lan>
2024-01-04 19:34:36 -0500

    v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='poptimizer',
-    version='0.0.3',
+    version='0.0.4',
     description='Utilize Large Language Models to Automatically Optimize Prompts for LLM Application Development',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
